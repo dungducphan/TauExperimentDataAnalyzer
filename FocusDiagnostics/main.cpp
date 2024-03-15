224e78330419e491881b5473a20aad5769f8b6a0
dungducphan <brianp.dung@utexas.edu>
2024-03-15 04:53:45 +0000

    Adding EPICS base libraries.

--- FocusDiagnostics/main.cpp
+++ FocusDiagnostics/main.cpp
@@ -1,8 +1,22 @@
 #include <QApplication>
 #include <focusdiagnosticsmainwindow.h>
 
+#include <iostream>
+#include <pva/client.h>
+
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    try {
+        if (argc <= 1) {
+            std::cerr << "Usage: " << argv[0] << " <pvname>\n";
+        }
+        pvac::ClientProvider provider("pva");
+        pvac::ClientChannel channel(provider.connect(argv[1]));
+        std::cout << channel.name() <<" : "<< channel.get() << "\n";
+    } catch (std::exception& e){
+        std::cerr << "Error: " << e.what() << "\n";
+    }
 
     FocusDiagnosticsMainWindow w;
     w.show();
