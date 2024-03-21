dbb2687915c1d5954fe7c7a8964a65ac4d8f01a1
dungducphan <brianp.dung@utexas.edu>
2024-03-21 21:10:08 +0000

    Testing icons.

--- FocusDiagnostics/main.cpp
+++ FocusDiagnostics/main.cpp
@@ -1,16 +1,17 @@
 // Qt6
 #include <QApplication>
-#include <memory>
+#include <QIcon>
 
 // EPICS
 #include <pva/client.h>
 
 // Custom
-#include "focusdiagnosticsmainwindow.h"
-#include "Controller.h"
+#include <focusdiagnosticsmainwindow.h>
+#include <Controller.h>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/icons/Resources/Icons/icons8-laser-48.png"));
 
     auto controller = new Controller();
     controller->ShowView();
