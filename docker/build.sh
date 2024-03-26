0ffe0dd52ddb14bceef7690bb3e80ec89cae3001
Dung D. Phan <brianp.dung@utexas.edu>
2024-03-25 19:13:00 -0500

    Make multi-stage build.

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build --tag=ddp822/teda:1.0.0 --target=teda-build .
+docker build --target=teda-build .
+docker build --tag=ddp822/teda:1.0.0 --target=teda-deploy .
+# docker push ddp822/teda:1.0.0
---
+++ docker/run.sh
@@ -0,0 +1,2 @@
+docker run -it --privileged --rm --net=host -e "DISPLAY=$DISPLAY" ddp822/teda:1.0.0
+
